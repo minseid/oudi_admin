cb231f23ad57b001f402f926e658644d17a05de1
Kim MinSeo <86477146+minseid@users.noreply.github.com>
2025-07-03 13:04:10 +0900

    ddd

--- oudi_admin/src/lib/db.ts
+++ oudi_admin/src/lib/db.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',      // DB 주소
-  user: 'root',           // DB 사용자
-  password: '비밀번호',   // DB 비밀번호
-  database: '데이터베이스명', // DB 이름
+  host: 'localhost',      
+  user: 'root',           
+  password: '1234',   
+  database: '', 
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
